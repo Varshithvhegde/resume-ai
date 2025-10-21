72414ebb9290c9e9616318bd6d46c9ee1c445c52
Varshith V Hegde <varshithvh@gmail.com>
2025-10-21 18:14:09 +0530

    Refines middleware request path matching to improve clarity and accuracy. Updates comments to specify exclusions for image optimization and public image files, enhancing overall configuration readability.

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,10 +29,11 @@ export const config = {
     /*
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
-     * - _next/image, images (image optimization files)
+     * - _next/image (image optimization)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     * - $ (root)
+     * - public image files (png, jpg, jpeg, gif, webp)
+     * - root ($)
      */
-    "/((?!_next/static|_next/image|images|favicon.[ico|png]|sitemap.xml|robots.txt|$).*)",
+    "/((?!_next/static|_next/image|favicon\\.ico|sitemap\\.xml|robots\\.txt|.*\\.(png|jpg|jpeg|gif|webp)|$).*)",
   ],
 }
